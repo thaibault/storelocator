552fffa64380f04968471c545b0140aa7dade510
torben <t.sickert[~at~]gmail.com>
2016-07-15 07:49:23 +0200

    a

--- node_modules/modify-babel-preset/test/fixtures/one.js
+++
@@ -1,7 +0,0 @@
-var modify = require('../../');
-
-module.exports = modify('es2015', {
-	// nameDrops: false,
-	'transform-react-jsx': { jsxPragma:'h' },
-	'transform-es2015-typeof-symbol': { loose:true }
-});
