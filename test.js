7a4a60856ed183c2bf11e56dee39c37b12bf8169
torben <t.sickert[~at~]gmail.com>
2016-08-14 22:14:10 +0200

    Automatic update to new version.

--- test.js
+++ test.js
@@ -21,6 +21,7 @@ import type {$DomNode, $Deferred} from 'jQuery-tools'
 import type StoreLocator from './index'
 // endregion
 // region declaration
+declare var DEBUG:boolean
 declare var TARGET_TECHNOLOGY:string
 // endregion
 // region types
